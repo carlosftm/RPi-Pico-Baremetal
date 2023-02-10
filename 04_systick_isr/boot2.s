e92972de30a44ab93b5c87c67d799e098f9cdbb9
carlosftm <carlosftm@gmail.com>
2023-02-10 23:11:30 +0100

    04 systick with interrupt. Update on Readme

--- 04_systick_isr/boot2.s
+++ 04_systick_isr/boot2.s
@@ -35,13 +35,20 @@
 
 copyToRam:
     ;@ load 16 bytes from FLASH to RAM at a time
-    ldmia r4!,{r0-r3}
-    stmia r5!,{r0-r3}    
-    sub r6,#16
+    ldmia r4!, {r0-r3}
+    stmia r5!, {r0-r3}    
+    sub r6, #16
     bne copyToRam
 
     ;@ Jump to the main function
-    ldr r0,=0x20000101
+    ldr r1, =VTOR
+    ldr r0, =0x20000100;
+    str r0, [r1]
+
+    ldr r0, =0x20002000
+    mov sp, r0
+
+    ldr r0, =0x20000201;
     bx r0
 
 .set XIP_SSI_BASE,       0x18000000
@@ -50,5 +57,6 @@ copyToRam:
 .set XIP_SSI_SSIENR,     XIP_SSI_BASE + 0x08
 .set XIP_SSI_BAUDR,      XIP_SSI_BASE + 0x14
 .set XIP_SSI_SPI_CTRLR0, XIP_SSI_BASE + 0xF4
+.set VTOR,               0xE000ED08
 
 .end
--- 04_systick_isr/systick_isr.c
+++ 04_systick_isr/systick_isr.c
@@ -43,35 +43,67 @@
 #define CORTEX_SYST_RVR (CORTEX_BASE + 0xe014)
 #define CORTEX_SYST_CVR (CORTEX_BASE + 0xe018)
 
+/* Handle unconfigured interrupts*/
+void irqLoop( void )
+{
+    while ( 1 );
+}
+
+/* Handler of the Tick interrupt */
+void irqSysTick( void )
+{
+    PUT32( SIO_GPIO_OUT_XOR, ( 1 << 25 ) );  // XOR the LED pin
+}
+
+/* Vector Table (Linker script has been upodated) */
+__attribute__( ( used, section( ".vectors" ) ) ) void ( *vectors[] )( void ) =
+{
+    0,          //  0 stack pointer value (NA)
+    irqLoop,    //  1 reset (NA)
+    irqLoop,    //  2 NMI
+    irqLoop,    //  3 hardFault
+    0,          //  4 reserved
+    0,          //  5 reserved
+    0,          //  6 reserved
+    0,          //  7 reserved
+    0,          //  8 reserved
+    0,          //  9 reserved
+    0,          // 10 reserved
+    irqLoop,    // 11 SVCall
+    0,          // 12 reserved
+    0,          // 13 reserved
+    irqLoop,    // 14 pendSV
+    irqSysTick, // 15 sysTick
+};
+
 /* Setup XOSC and set it a source clock */
-static void setupClocks(void)
+static void setupClocks( void )
 {
     // Enable the XOSC
-    PUT32(XOSC_CTRL, 0xAA0);            // Frequency range: 1_15MHZ
-    PUT32(XOSC_STARTUP, 0xc4);          // Startup delay ( default value )
-    PUT32((XOSC_CTRL | SET), 0xFAB000); // Enable ( magic word )
-    while (!(GET32(XOSC_STATUS) & 0x80000000)); // Oscillator is running and stable
+    PUT32( XOSC_CTRL, 0xAA0 );                        // Frequency range: 1_15MHZ
+    PUT32( XOSC_STARTUP, 0xc4 );                      // Startup delay ( default value )
+    PUT32( ( XOSC_CTRL | SET ), 0xFAB000 ) ;          // Enable ( magic word )
+    while ( !( GET32( XOSC_STATUS ) & 0x80000000 ) ); // Oscillator is running and stable
 
     // Set the XOSC as source clock for REF, SYS and Periferals
-    PUT32(CLK_REF_CTRL, 2);                     // CLK REF source = xosc_clksrc
-    PUT32(CLK_SYS_CTRL, 0);                     // CLK SYS source = clk_ref
-    PUT32(CLK_REF_DIV, (1 << 8));               // CLK REF Divisor = 1
-    PUT32(CLK_PERI_CTRL, (1 << 11) | (4 << 5)); // CLK PERI Enable & AUX SRC = xosc_clksrc
+    PUT32( CLK_REF_CTRL, 2 );                          // CLK REF source = xosc_clksrc
+    PUT32( CLK_SYS_CTRL, 0 );                          // CLK SYS source = clk_ref
+    PUT32( CLK_REF_DIV, ( 1 << 8 ) );                  // CLK REF Divisor = 1
+    PUT32( CLK_PERI_CTRL, ( 1 << 11 ) | ( 4 << 5 ) );  // CLK PERI Enable & AUX SRC = xosc_clksrc
 }
 
 /* reset the subsystems used in this program */
 static void resetSubsys()
 {
     // Reset IO Bank
-    PUT32((RESETS_RESET | CLR), (1 << 5));
-    while (GET32(RESETS_RESET_DONE) & (1 << 5) == 0);
+    PUT32(( RESETS_RESET | CLR ), ( 1 << 5 ) );
+    while ( GET32(RESETS_RESET_DONE ) & ( 1 << 5 ) == 0 );
 }
-
 
 /* ***********************************************
  * Main function
  * ********************************************* */
-__attribute__((used, section(".boot.entry"))) int main(void)
+__attribute__( ( used, section( ".boot.entry" ) ) ) int main( void )
 {
     // Setup clocks (XOSC as source clk)
     setupClocks();
@@ -79,19 +111,16 @@ __attribute__((used, section(".boot.entry"))) int main(void)
     resetSubsys();
 
     // Set GPIO25 as SIO ( F5) and GPIO OE
-    PUT32((IO_BANK0_GPIO25_CTRL), 5);
-    PUT32(SIO_GPIO_OE_SET, (1 << 25));
+    PUT32( ( IO_BANK0_GPIO25_CTRL ), 5 );
+    PUT32( SIO_GPIO_OE_SET, ( 1 << 25 ) );
 
-#define COUNT_250MS 12000000/4                       // with XOSC (12MHZ), 12,000,000 ticks = 1 second. -> 250ms = 3,000,000 ticks
-    PUT32(CORTEX_SYST_RVR, COUNT_250MS );            // start counting down from COUNT_250MS (3,000,000 ticks)
-    PUT32(CORTEX_SYST_CSR, ( 1 << 2) | ( 1 << 0 ) ); // source clock external / enable 
-    while(1)
+#define COUNT (12000000 / 32)                                       // with XOSC (12MHZ), 12,000,000 ticks = 1 second.
+    PUT32( CORTEX_SYST_RVR, COUNT );                                // start counting down from COUNT(12,000,000/16 ticks)
+    PUT32( CORTEX_SYST_CSR, ( 1 << 2 ) | ( 1 << 1 ) | ( 1 << 0 ) ); // source clock external / enable tick int / enable
+    while ( 1 )
     {
-        if ( GET32(CORTEX_SYST_CSR) & (1 << 16) )    // Check for the count flag (count compleated)
-        {
-            PUT32(SIO_GPIO_OUT_XOR, (1 << 25));      // XOR the LED pin
-        }
+        // loop here and wait for the tick interrupt
     }
 
-    return (0);
+    return ( 0 );
 }
